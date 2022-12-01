cca994c556ccecc33b767ad5840decd36718c227
VS130300 <codingtime@gmail.com>
2022-12-01 08:52:57 +0100

    Committed!

--- src/main/java/com/example/bigxpbowl/Employee/Service/EmployeeService.java
+++ src/main/java/com/example/bigxpbowl/Employee/Service/EmployeeService.java
@@ -1,6 +1,24 @@
 package com.example.bigxpbowl.Employee.Service;
 
+import com.example.bigxpbowl.Employee.Model.EmployeeModel;
+import org.springframework.data.repository.CrudRepository;
+
 public class EmployeeService
 {
+    private final CrudRepository<EmployeeModel, Long> repository;
 
+    public EmployeeService(CrudRepository<EmployeeModel, Long> repository)
+    {
+        this.repository = repository;
+    }
+
+    public Iterable<EmployeeModel> showAllEmployees()
+    {
+        return repository.findAll();
+    }
+
+    public EmployeeModel create(EmployeeModel employee)
+    {
+        return repository.save(employee);
+    }
 }
--- src/main/java/com/example/bigxpbowl/Reservation/Service/ReservationService.java
+++ src/main/java/com/example/bigxpbowl/Reservation/Service/ReservationService.java
@@ -1,5 +1,24 @@
 package com.example.bigxpbowl.Reservation.Service;
+
+import com.example.bigxpbowl.Reservation.Model.ReservationModel;
+import org.springframework.data.repository.CrudRepository;
+
 public class ReservationService
 {
+    private final CrudRepository<ReservationModel, Long> repository;
 
+    public ReservationService(CrudRepository<ReservationModel, Long> repository)
+    {
+        this.repository = repository;
+    }
+
+    public Iterable<ReservationModel> showAllReservations()
+    {
+        return repository.findAll();
+    }
+
+    public ReservationModel create(ReservationModel reservation)
+    {
+        return repository.save(reservation);
+    }
 }
